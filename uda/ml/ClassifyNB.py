eb23e720966186eb5e430cdebd48582670d0308f
Fei Wan <murphywan@gmail.com>
2017-02-04 21:45:16 +0800

    Create ClassifyNB.py

---
+++ uda/ml/ClassifyNB.py
@@ -0,0 +1,10 @@
+def classify(features_train, labels_train):   
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+    
+    
+    ### your code goes here!
+    
+    
